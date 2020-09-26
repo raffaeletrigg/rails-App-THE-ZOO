4dd1c37dbd25364e48fe1d56afb50bc91a2f64cf
raffaele triggiano <triggiano.r@gmail.com>
2020-09-26 19:09:30 +0200

    adding CUSTOM MESSAGES to USER Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # adding validations
-  validates :first_name, presence: true, length: { minimum: 2, too_short: "2 charachters is the minimum allowed" }
-  validates :last_name, presence: true, length: { minimum: 3, too_short: "2 charachters is the minimum allowed" }
-  validates :age, presence: true, numericality: { greater_than_or_equal_to: 18, }
+  validates :first_name, presence: { message: "first name must be given please" }, length: { minimum: 2, too_short: "2 charachters is the minimum allowed" }
+  validates :last_name, presence: { message: "last name must be given please" }, length: { minimum: 3, too_short: "2 charachters is the minimum allowed" }
+  validates :user_name, presence: { message: "username must be given please" }, uniqueness: { case_sensitive: false }, length: { minimum: 3, too_short: "2 charachters is the minimum allowed" }
+  validates :age, presence: { message: "username must be given please" }, numericality: { greater_than_or_equal_to: 18, }
 end
