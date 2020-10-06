8d170bba4c6bfda3e1aa3cb4ef7b14c6b440a213
raffaele triggiano <triggiano.r@gmail.com>
2020-10-06 20:31:47 +0200

    Adding ANIMAL CONTROLLER and index,show method + index view only

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'animals', to: 'animals#index'
+  get "animals/:id", to: "animals#show", as: :animal
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the how to customize you REDIRECT HOOKS "after singin or sign out", see https://github.com/heartcombo/devise#strong-parameters
 end
