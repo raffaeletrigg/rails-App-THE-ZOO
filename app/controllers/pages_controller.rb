86190b144ef4d6c2516948f309a3559230468bd1
raffaele triggiano <triggiano.r@gmail.com>
2020-10-06 19:26:41 +0200

    about page mvp + navbar logo link to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about ]
 
   def home
+  end
+
+  def about
+    @members = [ 'raffaele', 'michele', 'serena', 'nicoloo', 'antonella']
+
+    # How to keep the search context using params in your input?
+    # read this https://kitt.lewagon.com/camps/432/lectures/05-Rails%2F01-Rails-intro#/0/10/3
+    # the next if statement is the implementation
+    # for the rest of the implementation check the about.html.erb page
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
   end
 end
